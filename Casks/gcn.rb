3b5520a9a483a8f1b67e993344a965037aa028f0
Brahim Haddou <brahim.haddou@oracle.com>
2024-01-25 11:52:54 +0100

    remove deprecated homebrew appcast

--- Casks/gcn.rb
+++ Casks/gcn.rb
@@ -1,24 +1,23 @@
 cask 'gcn' do
     arch arm: 'aarch64', intel: 'amd64'
-   
+
     version '4.0.7'
     sha256 arm:   '1acfc2b7a7537e6956603f72ae7c39c95e3b9c7a1574684f63b2cc315e9d770d',
            intel: 'fc1cc35749be9a9ffd2c9e57bfd0665fb6651e9de6ea416f3df00ace4b04109b'
-  
+
     url "https://github.com/oracle/gcn/releases/download/#{version}/gcn-cli-#{version}-macos-#{arch}.tar.gz"
-    appcast 'https://github.com/oracle/gcn/releases.atom'
     name 'Graal Cloud Native'
     homepage 'https://www.graal.cloud/gcn/'
-  
+
     binary "gcn-cli-#{version}-macos-#{arch}/gcn"
     caveats <<~EOS
       On macOS Catalina or later, you may get a warning when you use the Graal Cloud
       Native installation for the first time. This warning can be disabled by running
       the following command:
         sudo xattr -d com.apple.quarantine "#{staged_path}/gcn-cli-#{version}-macos-#{arch}/gcn"
-  
+
       Graal Cloud Native is licensed under the Apache License Version 2.0:
         https://github.com/oracle/gcn/blob/main/LICENSE.txt
-  
+
     EOS
 end
