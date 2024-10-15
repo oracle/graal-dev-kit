27953c01d7db553c56a4f4071ad148e6112d3918
yassine_hadry <yassine.hadry@oracle.com>
2024-10-15 09:06:13 +0100

    update gdk brew cask

--- Casks/gdk.rb
+++ Casks/gdk.rb
@@ -2,8 +2,8 @@ cask 'gdk' do
     arch arm: 'aarch64', intel: 'amd64'
 
     version '4.6.0.3'
-    sha256 arm:   '02f4c60235e0bd9c6e8cc28982e7785f6131fcaa1766e68b850ba82653ac6428',
-           intel: '003096b853b3fa070a7926c04f2e6d87230ae391f26b962ce72b9be37607395d'
+    sha256 arm:   '01c1a3bf96ffc6d14a27c68fd5c6981008846d2b6df7bf2dce10be567057d0c5',
+           intel: '06a42eac544f154f3c0f1734a588e469b1ef1fc3fc292b2ca020c93b75a2b8dc'
 
     url "https://github.com/oracle/graal-dev-kit/releases/download/#{version}/gdk-cli-#{version}-macos-#{arch}.tar.gz"
     name 'Graal Development Kit for Micronaut'
