48ae1e92e17b32e90adaee04154e22ac15f2ead3
Burt Beckwith <burt.beckwith@oracle.com>
2025-07-15 09:42:12 -0400

    v4.7.3.5 release (#47)

--- gdk-cli-core/src/main/java/cloud/graal/gdk/AbstractStarter.java
+++ gdk-cli-core/src/main/java/cloud/graal/gdk/AbstractStarter.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2017-2022 original authors
- * Copyright 2024 Oracle and/or its affiliates
+ * Copyright 2025 Oracle and/or its affiliates
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
