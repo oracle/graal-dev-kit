d8dbad8f26977aa0b26b772f10f59a53d68b4bc0
yassine_hadry <yassine.hadry@oracle.com>
2025-09-23 10:13:41 +0100

    Update homebrew release to 4.7.3.7

--- Casks/gdk.rb
+++ Casks/gdk.rb
@@ -1,9 +1,9 @@
 cask 'gdk' do
     arch arm: 'aarch64', intel: 'amd64'
 
-    version '4.9.1.2'
-    sha256 arm:   'f2223ab53bf3e9731136933f6725aaa80d65ce5619a0613a7b70f93ff94b08a4',
-           intel: 'f67db8088fbb78b860978339af047843dd7a01b12e7aa1bed978400a68536a4b'
+    version '4.7.3.7'
+    sha256 arm:   'f679ae099396859f5098e8ce93144c468c57c90f174b9c8944436247a94addde',
+           intel: '89ffa8249cf3f9b5209d396d91b7899fffbb5b51d97a0664697f8318623fda2f'
 
     url "https://github.com/oracle/graal-dev-kit/releases/download/#{version}/gdk-cli-#{version}-macos-#{arch}.tar.gz"
     name 'Graal Development Kit for Micronaut'
