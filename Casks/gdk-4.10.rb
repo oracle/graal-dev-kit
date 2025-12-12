46b72bf5a37c88a0a73fb4a46211889d7a3546cb
Burt Beckwith <burt.beckwith@oracle.com>
2025-12-12 07:45:38 -0500

    4.10.1.1 release (#64)
    4.10.1.1 release

--- Casks/gdk-4.10.rb
+++ Casks/gdk-4.10.rb
@@ -1,9 +1,9 @@
 cask 'gdk-4.10' do
     arch arm: 'aarch64', intel: 'amd64'
 
-    version '4.10.1.0'
-    sha256 arm:   'f4df111295bb162cd8f88af3eee311c0d18d0d9e3e81d139c44fa3b5a8a452fb',
-           intel: 'f44e37025b13c4b9bc202602a4bc0970ffb35f7cd0ba7e3b1fb2be50ef64267e'
+    version '4.10.1.1'
+    sha256 arm:   'bd789603c41ca723d19f4ae4f767cf1dc344b32c5db7706195f88866d9cdfdd4',
+           intel: '3cc86f851aeb4252f572d55c5ff16914accb98bf140622d07631835d752e1e14'
 
     url "https://github.com/oracle/graal-dev-kit/releases/download/#{version}/gdk-cli-#{version}-macos-#{arch}.tar.gz"
     name 'Graal Development Kit for Micronaut'
