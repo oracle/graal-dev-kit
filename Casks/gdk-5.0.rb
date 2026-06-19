c06567b28359350a6da82457ae4d47323cdff9c3
Issam Liqali <liqali.issam@oracle.com>
2026-06-19 15:12:30 +0100

    5.0.2 homebrew (#77)

---
+++ Casks/gdk-5.0.rb
@@ -0,0 +1,22 @@
+cask 'gdk-5.0' do
+    arch arm: 'aarch64'
+
+    version '5.0.2'
+    sha256 arm:   '8b40120b4685c0cd73f7944bb60eb3f11a63350ca208b033e5dc5ed1ebb99280'
+
+    url "https://github.com/oracle/graal-dev-kit/releases/download/#{version}/gdk-cli-#{version}-macos-#{arch}.tar.gz"
+    name 'Graal Development Kit for Micronaut'
+    homepage 'https://graal.cloud/gdk/'
+
+    binary "gdk-cli-#{version}-macos-#{arch}/gdk"
+    caveats <<~EOS
+      On macOS Catalina or later, you may get a warning when you use the Graal Cloud
+      Native installation for the first time. This warning can be disabled by running
+      the following command:
+        sudo xattr -d com.apple.quarantine "#{staged_path}/gdk-cli-#{version}-macos-#{arch}/gdk"
+
+      Graal Development Kit for Micronaut is licensed under the Apache License Version 2.0:
+        https://github.com/oracle/graal-dev-kit/blob/main/LICENSE.txt
+
+    EOS
+end
